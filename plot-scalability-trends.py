820b6a26a11ccce01c63737509309b21d1084329
Omar Rizwan <omar.rizwan@gmail.com>
2017-05-28 18:34:36 -0700

    Plot scalability trends.

--- plot-scalability-trends.py
+++ plot-scalability-trends.py
@@ -1,10 +1,11 @@
-# ssh-copy-id so doesn't need password.
+# should ssh-copy-id so doesn't need password.
 
 from __future__ import print_function
 import sys
 import tempfile
 import itertools
 from subprocess import check_output, PIPE
+import matplotlib.pyplot as plt
 
 if len(sys.argv) != 2:
     print('Usage:', sys.argv[0], 'MACHINE')
@@ -25,21 +26,27 @@ machine = sys.argv[1]
     
 
 def run_on_machine(command):
-    return check_output(['ssh', machine,
-                         'cd ~/pyretic/pyretic/evaluations/evaluation_results/nsdi16; ' + command])
+    results = check_output(['ssh', machine,
+                            'cd ~/pyretic/pyretic/evaluations/evaluation_results/nsdi16; ' + command])
+    return [result.split(' ') for result in results.splitlines()]
+
+plt.xlabel('Number of nodes')
 
 getcompiletimes = r'''for i in `echo 20 40 60 80 100 120 140 160`; do for test in `echo igen_delauney waxman_03_03 waxman_02_04 waxman_04_02 waxman_05_015`; do for query in `echo tm congested_link path_loss slice ddos firewall`; do grep -H 'total time' ${query}_${test}_${i}_fdd_1/stat.txt | awk '{print $1 " " $4}' | sort -nk2 | tail -1; done ; done | awk -v prefix=$i '{total += $2; count++} END { print prefix " " total/count " " count }'  ; done 2>/dev/null'''
 compiletimes = run_on_machine(getcompiletimes)
-print(compiletimes)
+sizes, avgcompiletimes, counts = zip(*compiletimes)
+plt.plot(sizes, avgcompiletimes, '-o', label='Avg compile time (s)')
 
 # get rule count for in_table, averaged
 getrulecounts = r'''for i in `echo 20 40 60 80 100 120 140 160`; do for test in `echo igen_delauney waxman_03_03 waxman_02_04 waxman_04_02 waxman_05_015`; do for query in `echo tm congested_link path_loss slice ddos firewall`; do grep -H 'in_table' ${query}_${test}_${i}_fdd_1/stat.txt | grep '))' | sed '/^S/d' | awk '{print $1 " " $3}' | sed 's/.$//' | awk '{ total += $2 ; count++ } END { print $1 " " total " " count }' ; done ; done  | sed '/^\s\s$/d' | awk -v prefix=$i '{ total += $2; count++ } END { print prefix " " total/count }' ; done 2>/dev/null'''
 rulecounts = run_on_machine(getrulecounts)
-print(rulecounts)
+sizes, avgrulecounts = zip(*rulecounts)
+plt.plot(sizes, avgrulecounts, '-o', label='Avg rule count')
+plt.legend()
+plt.title('Scalability trends on synthetic ISP topologies (%s)' % (machine))
+plt.show()
 
 # get state count, averaged
 getstatecounts = r'''for i in `echo 20 40 60 80 100 120 140 160`; do for test in `echo igen_delauney waxman_03_03 waxman_02_04 waxman_04_02 waxman_05_015`; do for query in `echo tm congested_link path_loss slice ddos firewall`; do val=`grep -H 'state count' ${query}_${test}_${i}_fdd_1/stat.txt | awk '{printf("%s ", $4)}' ` ; python -c "import math; sum=reduce(lambda acc,x: acc + int(math.ceil(math.log(int(x),2))), \"${val}\".split(), 0); print sum" ; done ; done | awk -v prefix=$i ' { if ( $1 > 0 ) { total += $1; count++ } } END { print prefix " " total/count " " count }' ; done 2>/dev/null'''
 statecounts = run_on_machine(getstatecounts)
-print(statecounts)
 
-# FIXME: run through plotter.
