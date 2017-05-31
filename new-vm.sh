64e515626eb1761dca5d2dd2994e81910c06857c
Omar Rizwan <omar.rizwan@gmail.com>
2017-05-30 23:51:52 -0700

    Script that properly sets up VM.

--- new-vm.sh
+++ new-vm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -eux
 gcloud compute instances create $1 --image pyretic-clean
 gcloud compute scp pyretic-clean-to-path-queries.sh mininet@$1:~
-gcloud compute ssh mininet@$1 -- './pyretic-clean-to-path-queries.sh'
+
+# Start the dep install script in screen, so we don't have to babysit it.
+gcloud compute ssh mininet@$1 -- 'screen -L -S to-path-queries "/home/mininet/pyretic-clean-to-path-queries.sh"'
