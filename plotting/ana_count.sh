7584a68ae96cf018fd983b99b9e2e64da7342782
Dom Piens <dom@Doms-MacBook-Air.local>
2017-06-03 01:30:08 -0700

    added script to generate csv from stat.txt files, then plot data by query and for lumped queries.

---
+++ plotting/ana_count.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for i in `echo 20 40 60 80 100 120 140 160`; do 
+	for test in `echo igen_delauney waxman_03_03 waxman_02_04 waxman_04_02 waxman_05_015`; do
+		for q in `echo tm congested_link path_loss slice ddos firewall`; do
+		    for cnt in `seq 1 5`; do
+				j=`echo $i, $test, $q, $cnt`
+				grep -H 'in_table' ${q}_${test}_${i}_fdd_${cnt}/stat.txt | grep '))' | sed '/^S/d' | awk '{print $1 " " $3}' | sed 's/.$//' | awk -v var="$j" '{printf("%s, %s\n", var, $2)}'
+		    done 2>/dev/null
+		done;
+	done
+done;
---
+++ plotting/gen_csv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./ana_time.sh > ana_time.csv
+./ana_count.sh > ana_count.csv
