3e8fdd205d88a6ecddc2c114088aebc25c9de369
Omar Rizwan <omar.rizwan@gmail.com>
2017-06-03 08:56:30 -0700

    Fix copy-not-clone and refactor.
    Replicator should really only care about replicate/ folder, and maybe
    also test/ if they want to tweak the tests.

--- replicate/replicate-in-vm.sh
+++ replicate/replicate-in-vm.sh
@@ -5,8 +5,14 @@ set -o pipefail
 
 # Copy the scripts in this repo into the VM, then run the replication.
 
-# ssh will ask for password:
+# ssh will ask for password twice:
 # mininet
 
-gcloud compute scp --recurse .. mininet@path-queries-replicator:~/path-queries --zone asia-east1-a
-gcloud compute ssh mininet@path-queries-replicator --zone asia-east1-a -- 'cd path-queries && screen -L -S rep-tests sudo bash rep_tests.sh'
+cd ..
+tar czf - * | gcloud compute ssh mininet@path-queries-replicator --zone asia-east1-a -- 'mkdir path-queries && cd path-queries && tar xvzf -'
+cd -
+
+gcloud compute ssh mininet@path-queries-replicator --zone asia-east1-a -- 'cd path-queries && screen -L -S rep-tests sudo bash test/rep_tests.sh'
+
+# Output log (even if you detach) will be in ~/path-queries/screenlog.0 on the VM.
+# (NOT ~/screenlog.0!)
