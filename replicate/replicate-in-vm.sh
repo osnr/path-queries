fcaf8507753e5186a316a707f7a7d358fe226d27
Omar Rizwan <omar.rizwan@gmail.com>
2017-06-03 08:40:22 -0700

    Copy, not clone, to make workload easier to edit.

--- replicate/replicate-in-vm.sh
+++ replicate/replicate-in-vm.sh
@@ -3,9 +3,10 @@ set -o pipefail
 
 # Run this script after running vm-from-image.sh.
 
-# Clone our final scripts into the VM, then run the replication.
+# Copy the scripts in this repo into the VM, then run the replication.
 
 # ssh will ask for password:
 # mininet
 
-gcloud compute ssh mininet@path-queries-replicator --zone asia-east1-a -- 'git clone https://github.com/osnr/path-queries && cd path-queries && screen -L -S rep-tests sudo bash rep_tests.sh'
+gcloud compute scp --recurse .. mininet@path-queries-replicator:~/path-queries --zone asia-east1-a
+gcloud compute ssh mininet@path-queries-replicator --zone asia-east1-a -- 'cd path-queries && screen -L -S rep-tests sudo bash rep_tests.sh'
